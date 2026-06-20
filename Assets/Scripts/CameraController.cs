737c70506dd3784e9a51297b7c4afb0e471ec8ee
Yuji.N <119279870+yuji-N3568@users.noreply.github.com>
2026-06-20 18:11:05 +0900

    Some Change Camera and Player
    ・カメラをマウス操作に対応
    ・カメラの向きにキーボード操作を対応
    ・反発を追加
    ・NPの移動を一旦削除
    ・プレイヤーの操作を調整

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;   // プレイヤーの追従
-
+    
+    [SerializeField] private float mouseSensitivity = 0.1f; // マウス感度
     private Vector3 offset;
+    private float yaw;  // x軸回転
+    private float pitch;    // y軸回転
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;    // プレイヤーとの位置関係を固定
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();   // マウス入力を取得
+
+        yaw += mouseDelta.x * mouseSensitivity;
+        pitch -= mouseDelta.y * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, -20f, 20f);  // 真上・真下を向いてひっくり返るのを防ぐ
+
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f); // 回転を適用
+
+        transform.position = player.transform.position + rotation * offset;    // プレイヤーとの距離を固定
+        transform.LookAt(player.transform); //
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,29 +3,65 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed; 
+    [SerializeField] private float speed = 10.0f;     // 加速の大きさ
+    [SerializeField] private float maxSpeed = 10.0f;  // 最大速度
+    [SerializeField] private float impactTime = 0.5f;  // 衝突してからふっとぶ時間
 
     private Rigidbody rb; 
     private float movementX;
     private float movementY;
+    
+    private bool impact = false;   // 衝突判定
+    private float impactTimer = 0f;      // 時間測定用
 
     void Start()
     {
-        rb = GetComponent <Rigidbody>(); 
+        rb = GetComponent <Rigidbody>();    // 物理演算を取得
     }
 
     void OnMove(InputValue movementValue)
     {
+        // 入力からベクトルを得る
         Vector2 movementVector = movementValue.Get<Vector2>(); 
 
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        movementX = movementVector.x;   // 左右方向(A, Dキー) 
+        movementY = movementVector.y;   // 前後方向(W, Sキー)
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        impact = true;
+        impactTimer = impactTime;
     }
 
     void FixedUpdate() 
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed); 
-    }
+        Vector3 cameraX = Camera.main.transform.right;  // カメラ基準の左右方向
+        Vector3 cameraY = Camera.main.transform.forward;    // カメラ基準の前後方向
 
+        // y方向(上下)を無効化し、正規化(向きの基準であるため)
+        cameraX.y = 0;
+        cameraY.y = 0;
+        cameraX.Normalize();
+        cameraY.Normalize();
+
+        // x方向とz方向に入力を反映させ、正規化(x+zが大きくなるため)
+        Vector3 movement = cameraX * movementX + cameraY * movementY;   
+        movement.Normalize();
+
+        if(impact)  // 衝突しているとき
+        {
+            impactTimer -= Time.fixedDeltaTime; // 経過時間を減算
+
+            if(impactTimer <= 0)
+            {
+                impact = false; // 衝突処理を終了
+            }
+        }
+        else
+        {
+            rb.AddForce(movement * speed);  // Playerに力を加える(=加速する)
+            rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxSpeed);    // 最大速度の処理
+        }
+    }
 }
